19bb5b5533f941525e1d944c52bfcdb2545fb214
Srinivasan Rangarajan <srini6773@gmail.com>
2018-04-14 14:50:51 -0700

    course

---
+++ app/src/main/java/com/example/srini/slugtutor/Course.java
@@ -0,0 +1,31 @@
+package com.example.srini.slugtutor;
+
+public class Course {
+    private String courseNum;
+    private String professor;
+    private String description;
+
+    public Course(String courseNum, String professor, String description)
+    {
+        this.courseNum=courseNum;
+        this.professor=professor;
+        this.description=description;
+    }
+
+    public String getCourseNum()
+    {
+        return courseNum;
+    }
+
+    public String getDescription()
+    {
+        return description;
+    }
+
+    public String getProfessor()
+    {
+        return professor;
+    }
+
+
+}
--- app/src/main/java/com/example/srini/slugtutor/MainActivity.java
+++ app/src/main/java/com/example/srini/slugtutor/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.srini.slugtutor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.AsyncTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private class Parser extends AsyncTask<Void, Void, Void> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+
+        }
     }
 }
